db07217375b8bc70b330bbf1bb3ad663749bf27d
jessi68 <lynn312.unist.ac.kr>
2021-08-13 22:05:54 +0900

    feat: 검색창 컴포넌트 구현

---
+++ src/js/components/Youtube/Search.js
@@ -0,0 +1,14 @@
+class SearchInput extends Component {
+    // <form class="d-flex">
+    //         <input type="text" class="w-100 mr-2 pl-2" placeholder="검색" />
+    //         <button type="button" class="btn bg-cyan-500">검색</button>
+    //       </form>
+    constructor(props) {
+        super(props);
+        this.renderComponent();
+        this.$target.appendChild(this.$element);
+    }
+
+    render() {
+    }
+}
